505d899ca388a87f312b5000285540a7a7a3ae28
ERKCreeper <117591514+ERKCreeper@users.noreply.github.com>
2022-11-06 13:45:32 -0500

    Update code.js

--- DumbMusic/Version 1/code.js
+++ DumbMusic/Version 1/code.js
@@ -2,7 +2,7 @@ document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s"
 document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[0].style.borderWidth = "thick";
 document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[1].innerHTML = "Proof we dont know how to grade you.";
 document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[1].style.borderWidth = "thick";
-document.getElementsByClassName("sm-btn sm-btn-outlined play-button sm-btn-rounded sm-btn-m")[0].innerHTML = "Practice with DumbFeedback®";
+document.getElementsByClassName("sm-btn sm-btn-outlined play-button sm-btn-rounded sm-btn-m")[0].innerHTML = "Practice(But it still won't help you if<br>we don't know how to grade you.)";
 document.getElementsByClassName("sm-btn sm-btn-outlined play-button sm-btn-rounded sm-btn-m")[0].style.borderWidth = "thick";
 document.getElementsByClassName("sm-btn sm-btn-outlined record-button sm-btn-rounded sm-btn-m")[0].innerHTML = "Pathway to a False 0%";
 document.getElementsByClassName("sm-btn sm-btn-outlined record-button sm-btn-rounded sm-btn-m")[0].style.borderWidth = "thick";

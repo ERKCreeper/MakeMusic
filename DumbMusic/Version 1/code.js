03e65274f32a8f109991ffb41b0a745c1939020b
ERKCreeper <117591514+ERKCreeper@users.noreply.github.com>
2022-11-06 13:40:20 -0500

    Update code.js

--- DumbMusic/Version 1/code.js
+++ DumbMusic/Version 1/code.js
@@ -1,1 +1,8 @@
-
+document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[0].innerHTML = "Uncalibrate Microphone";
+document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[0].style.borderWidth = "thick";
+document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[1].innerHTML = "Proof we dont know how to grade you.";
+document.getElementsByClassName("sm-btn sm-btn-outlined sm-btn-rounded sm-btn-s")[1].style.borderWidth = "thick";
+document.getElementsByClassName("sm-btn sm-btn-outlined play-button sm-btn-rounded sm-btn-m")[0].innerHTML = "Practice with DumbFeedback®";
+document.getElementsByClassName("sm-btn sm-btn-outlined play-button sm-btn-rounded sm-btn-m")[0].style.borderWidth = "thick";
+document.getElementsByClassName("sm-btn sm-btn-outlined record-button sm-btn-rounded sm-btn-m")[0].innerHTML = "Pathway to a False 0%";
+document.getElementsByClassName("sm-btn sm-btn-outlined record-button sm-btn-rounded sm-btn-m")[0].style.borderWidth = "thick";
